0da6cf22cae568e889b123bbde6cac6a10a714a8
hansh bardhan <150507183+hbardhan18@users.noreply.github.com>
2024-09-29 21:53:03 +0530

    Create index.js

---
+++ Day-1/index.js
@@ -0,0 +1,23 @@
+//1.Datatypes
+//String
+console.log("hanshu", typeof("hanshu"))
+console.log('2468' , typeof('2468'));
+console.log(`@#$&%*` , typeof(`@#$%&*`));
+//number
+console.log(369, typeof (369));
+
+console.log(1034.78, typeof( 1034.78));
+//Boolean
+console.log(true , typeof (true));
+console.log(false , typeof (false));
+//Undefined
+console.log(typeof(book));
+//null
+//2.variable-store data
+//value assign
+//ressign
+//declare
+//3.operator
+//arithematic operator
+//
+//
