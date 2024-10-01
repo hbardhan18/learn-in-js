c2c870c16e55708c39ec279da4dc173c5070a39d
hansh bardhan <150507183+hbardhan18@users.noreply.github.com>
2024-10-01 16:38:10 +0530

    Update index.js

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,4 +1,42 @@
 //let
 //let-variable.
 // difference between var let and const:
-                          var      let      const
+ //             var      let      const
+// r
+let quanlity =   18;
+console.log(quanlity);
+quantity=2020;
+console.log(quantity);
+//let quantity-19;
+const myage=23;
+console.log(myage);
+//myage=28;
+//console.log(myage);
+//const myage=32;
+//console.log(myage);
+//{}-block
+var mynumber=9;
+console.log(mynumber);
+{
+    console.log(mynumber);
+}
+{
+    var mynumber=99;
+console.log(mynumber);
+}
+console.log(mynumber);
+{
+    let mynumber=2021;
+console.log(mynumber);
+
+{
+console.log(mynumber);
+}
+}
+//hoisting
+var myNumber;
+console.log(myNumber,typesOf(myNumber));
+        
+
+    
+    
