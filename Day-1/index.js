593fc3d050d7800ca9b068cedbf3d83cd370677e
hansh bardhan <150507183+hbardhan18@users.noreply.github.com>
2024-10-01 13:32:23 +0530

    update

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,5 +1,5 @@
 //1.Datatypes
-//String
+//String:'""`;
 console.log("hanshu", typeof("hanshu"))
 console.log('2468' , typeof('2468'));
 console.log(`@#$&%*` , typeof(`@#$%&*`));
@@ -13,11 +13,51 @@ console.log(false , typeof (false));
 //Undefined
 console.log(typeof(book));
 //null
-//2.variable-store data
-//value assign
+//Function:Multiple lines of codes.-features return
+//There are two types of function:1 inbulid
+//console.log("himanshu",'gaya',`123`);
+//2.variable-store data:
+//let
+//var
+//var kuch ho-store data
 //ressign
 //declare
+var myName="nikhil";
+console.log(myName);
+var myclass=("mca");
+console.log("class");
+var myage=(63);
+console.log(myage);
+var myage=(24);
+console.log(myage,"myage");
+var age=("28");
+console.log(age,age);
+var myage="25";
+console.log(myage,"myage");
+var number=14;//declare or assign
+console.log(number);
+var number=82;
+number=88;//re assign
+console.log(number);
+var number=48;
+console.log(number);
+var number=49;
+console.log(number);
+var number=50;
+console.log(number);
+//difference between var letand const:
+//              var
+// reassign     true
+// redeclare   true
+ var number1=30;
+ var number2=20;
+ console.log(number1+number2);
+ console.log(number1-number2);
+ console.log(number*1number2);
+console.log(number1/number2);
 //3.operator
 //arithematic operator
-//
-//
+//comparsion
+//=-to assign value;
+//==-comparsion data;
+//===-comparsion data and datatypes;
