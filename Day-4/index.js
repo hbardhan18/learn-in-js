9cdea7397f8b23f57e00efabf0af73e186fc3a49
hansh bardhan <150507183+hbardhan18@users.noreply.github.com>
2024-10-04 16:36:12 +0530

    Create index.js

---
+++ Day-4/index.js
@@ -0,0 +1,45 @@
+//Function
+//There are two types of function;
+//1.inbuilt function console.log() type()
+//we can create our own functions
+//Function-its provide functionality/usecase
+//Function-store multiple lines of codes.
+//    syntax:
+function type() {  
+    console.log("inside a function");
+} 
+type();
+console.log("outside a function");
+function addition() {
+var number1 = 78;
+var number2 = 57;
+console.log(number1 + number2);
+}
+addition();
+function findgreaternumber() {
+var number1 = 478966;
+var number2 = 93675;
+if(number1 > number2) {
+console.log("number 1 is greater than number 2");
+} else {
+    console .log("number 2 is greater than number 1");
+}
+}
+findgreaternumber();
+function substraction() {
+    console.log(34-12);
+}
+substraction();
+ //var number1 = 54;
+ //var number2 = 47;
+ //function subtraction(hi, hello) {
+//console.log("hi,hello,hi,hello") ;
+//}
+//subtraction(number1,number2);
+function multiplication(value1,value2) {
+    console.log(value1 * value2);
+}
+multiplication(28,54);
+//function multilpication(value1 ,value2) {
+//var answer = value* value2}
+
