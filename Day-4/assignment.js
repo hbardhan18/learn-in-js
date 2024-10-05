cc9cbe1ad509a43f32de97bbde73fb652881013e
hansh bardhan <150507183+hbardhan18@users.noreply.github.com>
2024-10-05 15:05:37 +0530

    update

---
+++ Day-4/assignment.js
@@ -0,0 +1,10 @@
+//create a function who return odd or even values, take single input as parameter .
+var number = 98;
+function evenodd() {
+    if (number % 2 == 0)  {
+        console.log("input ",number, "is Even number");
+    } else {
+        console.log("input ",number, "is Odd number");
+    }
+}
+evenodd();
