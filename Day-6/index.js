ec79015a9e45cf40b34c6dd3d59d5c8e81cb1a81
hansh bardhan <150507183+hbardhan18@users.noreply.github.com>
2024-10-09 21:00:17 +0530

    Create index.js

---
+++ Day-6/index.js
@@ -0,0 +1,128 @@
+// Loops
+// There are two types of loops:
+//.1 for loop, 2.while loop
+
+// print number from 1-10:
+// function printnumbers() {
+   // console.log(1)
+    // console.log(2)
+   // console.log(3)
+//console.log(4)
+// }
+// printnumbers()
+// for loop
+// Syntax;
+// for(starting : ending : sequence) {
+  //  multiple lines of Code
+// }
+// for(1:10:1) {
+
+// console.log()
+// }
+// for(var mynumber = 1 ; 10) {
+//}
+//var num = 1;
+//num = num + 1;
+//num ++; //increment by 1.
+//num += 3;
+//console.log(num);
+
+//for (i = 1 ; i < 11 ; i++) {
+    //console.log(i);
+//}
+// 1. iteration;
+// i = 1;
+//i <=10;
+// 1 <=10 true;
+// console.log(i ); value is 1 .
+// i++;
+// i = 2;
+
+
+// 2. iteration .
+// i = 2;
+// i <= 10;
+// 2 <=10 true;
+//console.log)(i) ; value is 2.
+// i++;
+// i = 3;
+
+
+// 3. iteration .
+// i = 3;
+// i <= 10;
+// 3 <= 10 true;
+//console.log)(i) ; value is 3.
+// i++;
+// i = 4;
+
+
+// 4. iteration .
+// i = 4;
+// i <=10;
+// 4 < =10 true;
+//console.log)(i) ; value is 4.
+// i++;
+// i = 5;
+
+
+// 5. iteration .
+// i = 5;
+// i <= 10;
+// 5 <= 10 true;
+//console.log)(i) ; value is 5.
+// i++;
+// i = 6;
+
+
+// 6. iteration .
+// i = 6;
+// i <= 10;
+// 6 <= 10 true;
+//console.log)(i) ; value is 6.
+// i++;
+// i = 7;
+
+
+// 7. iteration .
+// i = 7;
+// i <= 10;
+// 7 <=10 true;
+//console.log)(i) ; value is 7.
+// i++;
+// i = 8;
+
+
+// 8. iteration .
+// i = 8;
+// i <= 10;
+// 8 < =10 true;
+//console.log)(i) ; value is 8.
+// i++;
+// i = 9;
+
+
+// 9. iteration .
+// i = 9;
+// i <=10;
+// 9 <=10 true;
+//console.log)(i) ; value is 9.
+// i++;
+// i = 10;
+
+
+
+// 10. iteration .
+// i = 10;
+// i <= 10;
+// 10 <=10 true;
+//console.log)(i) ; value is 10.
+// i++;
+// i = 11;
+
+
+// 11. iteration .
+// i = 11;
+// i < =10;
+// 11 <= 10 ; then condition is false.
+// after loop is terminate for next line.
